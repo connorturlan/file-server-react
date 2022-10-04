254f1e7d6ab64dadc3dd8208e32ae1246c9363cd
Connor <connorturlan@gmail.com>
2022-10-04 17:10:46 +1030

    added video preview, and play on hover

--- src/components/FileIcon/FileIcon.jsx
+++ src/components/FileIcon/FileIcon.jsx
@@ -3,18 +3,46 @@ import { getFileURL } from "../../utils";
 import IconComponent from "../IconComponent/IconComponent";
 
 function FileIcon({ name, path, onClick, isPreview }) {
-	// generate the image preview if the file is an image.
-	const imagePreview = /.(jpe?g|gif|png|webp|webm)$/i.test(
-		name.toLowerCase()
-	) &&
-		isPreview && (
-			<img
-				className={styles.base__image}
-				src={getFileURL(path)}
-				title={name}
-			></img>
-		);
+	const playVideo = (event) => {
+		event.target.play();
+	};
 
+	const pauseVideo = (event) => {
+		event.target.pause();
+	};
+
+	// generate the image preview if the file is an image.
+	let imagePreview = false;
+	if (isPreview) {
+		if (/.(jpe?g|gif|png|webp)$/i.test(name.toLowerCase())) {
+			imagePreview = (
+				<img
+					className={styles.base__image}
+					src={getFileURL(path)}
+					title={name}
+				></img>
+			);
+		} else if (/.(mp4|webm|mov)$/i.test(name.toLowerCase())) {
+			imagePreview = (
+				<video
+					className={styles.base__video}
+					title={name}
+					onMouseOver={playVideo}
+					onMouseLeave={pauseVideo}
+					preload="metadata"
+					loop
+				>
+					<source src={getFileURL(path)} type="video/webm" />
+					<source src={getFileURL(path)} type="video/mp4" />
+				</video>
+			);
+		}
+	}
+
+	/* imagePreview.onMouseOver(() => {
+		imagePreview.play;
+	});
+ */
 	return (
 		<IconComponent styleClass={styles} onClick={onClick} path={path}>
 			{imagePreview || name}
